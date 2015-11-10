4fc355b3e2e677308202c589732fe5cb31bfbbd7
Carter Chang <carter@yahoo-inc.com>
2015-11-11 00:41:00 +0800

    complete base function

--- qrcoGenExtension/ActionViewController.m
+++ qrcoGenExtension/ActionViewController.m
@@ -8,10 +8,11 @@
 
 #import "ActionViewController.h"
 #import <MobileCoreServices/MobileCoreServices.h>
-
+#import <QRCodeGenerator.h>
 @interface ActionViewController ()
 
 @property(strong,nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIImageView *qrcodeView;
 
 @end
 
@@ -24,7 +25,7 @@
     
     // For example, look for an image and place it into an image view.
     // Replace this with something appropriate for the type[s] your extension supports.
-    BOOL imageFound = NO;
+    // BOOL imageFound = NO;
     for (NSExtensionItem *item in self.extensionContext.inputItems) {
         for (NSItemProvider *itemProvider in item.attachments) {
             
@@ -38,7 +39,8 @@
                     if (item) {
                         [[NSOperationQueue mainQueue] addOperationWithBlock:^{
                             NSLog(@"========URL %@==================", item.absoluteString);
-                
+                            self.qrcodeView.image = [QRCodeGenerator qrImageForString:item.absoluteString imageSize:self.qrcodeView.bounds.size.width];
+                            
                             
                         }];
                     }
@@ -46,29 +48,50 @@
             }
 
             
+//            if ([itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypePropertyList]) {
+//                
+//                [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypePropertyList options:nil completionHandler:^(NSDictionary *jsDict, NSError *error) {
+//                    
+//                    if (jsDict) {
+//                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//                            NSDictionary *jsPreprocessingResults = jsDict[NSExtensionJavaScriptPreprocessingResultsKey];
+//                            NSString *selectedText = jsPreprocessingResults[@"selection"];
+//                            NSString *pageTitle = jsPreprocessingResults[@"title"];
+//                            if ([selectedText length] > 0) {
+//                                //self.textView.text = selectedText;
+//                            } else if ([pageTitle length] > 0) {
+//                                //self.textView.text = pageTitle;
+//                            }
+//                        }];
+//                    }
+//
+//                    
+//                }];
+//            }
             
             
-            
-            if ([itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeImage]) {
-                // This is an image. We'll load it, then place it in our image view.
-                __weak UIImageView *imageView = self.imageView;
-                [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeImage options:nil completionHandler:^(UIImage *image, NSError *error) {
-                    if(image) {
-                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                            [imageView setImage:image];
-                        }];
-                    }
-                }];
-                
-                imageFound = YES;
-                break;
-            }
+//            if ([itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeImage]) {
+//                // This is an image. We'll load it, then place it in our image view.
+//                __weak UIImageView *imageView = self.imageView;
+//                [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeImage options:nil completionHandler:^(UIImage *image, NSError *error) {
+//                    if(image) {
+//                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//                            [imageView setImage:image];
+//                        }];
+//                    }
+//                }];
+//                
+//                imageFound = YES;
+//                break;
+//            }
+
+        
         }
         
-        if (imageFound) {
-            // We only handle one image, so stop looking for more.
-            break;
-        }
+//        if (imageFound) {
+//            // We only handle one image, so stop looking for more.
+//            break;
+//        }
     }
 }
 
